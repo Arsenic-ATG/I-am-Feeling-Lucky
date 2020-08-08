45dfde368d95960deb593a521a692e0cf4c08b38
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-08-08 18:09:44 +0530

    🔧 Fix syntax errors

--- lucky.py
+++ lucky.py
@@ -1,17 +1,17 @@
 #! Python4
 # lucky.py - searches google and open top results in new tabs
 
-import webbrowser,bs4,sys
+import webbrowser,bs4,sys,requests
 
 res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 # Retrieve top search result links.
-soup = bs4.BeautifulSoup(res)
+soup = bs4.BeautifulSoup(res.text,"html.parser")
 
 # Open a browser tab for each result
 link = soup.select('.r a')
 no_of_tabs = 5
 
-for i in range no_of_tabs:
+for i in range(no_of_tabs):
 	webbrowser.open("https://www.google.com" + link[i].get('href'))
