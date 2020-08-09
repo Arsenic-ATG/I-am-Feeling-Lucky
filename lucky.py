a35190db7a64f66000f46b6edf695b25ecdb07a3
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-08-09 12:41:02 +0530

    🔧Fix the empty list bug

--- lucky.py
+++ lucky.py
@@ -2,17 +2,19 @@
 
 import webbrowser,bs4,sys,requests
 
-res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+#res = requests.get('https://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res = requests.get('https://google.com/search?q=google')
 res.raise_for_status()
 
 # Retrieve top search result links.
-soup = bs4.BeautifulSoup(res.text,"html.parser")
-
+soup = bs4.BeautifulSoup(res.text,'lxml')
+# print(soup)
 # Open a browser tab for each result
-link = soup.select('.LC20lb DKV0Md a')
-
-#print(link)
+#link = soup.select('.r a')
+link = soup.select('div#main > div > div > div > a')
+print(link)
 no_of_tabs = min(5,len(link))
 
 for i in range(no_of_tabs):
 	webbrowser.open("https://www.google.com" + link[i].get('href'))
+
