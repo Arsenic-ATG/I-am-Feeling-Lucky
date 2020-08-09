e0f847d51529f9310d11de8cdb4f2c075b649529
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-08-09 12:44:07 +0530

    🛠  Remove debugger code

--- lucky.py
+++ lucky.py
@@ -2,17 +2,15 @@
 
 import webbrowser,bs4,sys,requests
 
-#res = requests.get('https://google.com/search?q=' + ' '.join(sys.argv[1:]))
-res = requests.get('https://google.com/search?q=google')
+# Get the address from the user
+res = requests.get('https://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 # Retrieve top search result links.
 soup = bs4.BeautifulSoup(res.text,'lxml')
-# print(soup)
+
 # Open a browser tab for each result
-#link = soup.select('.r a')
 link = soup.select('div#main > div > div > div > a')
-print(link)
 no_of_tabs = min(5,len(link))
 
 for i in range(no_of_tabs):
